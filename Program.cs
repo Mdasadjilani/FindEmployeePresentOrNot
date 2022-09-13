de35e855d1cf25909500b37dc6fa41011729a326
Mdasadjilani <asadjilani20@gmail.com>
2022-09-14 01:18:12 +0530

    FindEmployeePresentOrNot

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageCompute
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC-1 Find Employee is present or not
+            int empPresent = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+
+            if (empPresent==empCheck)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is absent");
+            Console.ReadLine();
+        }
+    }
+}
